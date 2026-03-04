7a4a1acdf6da88c72909a011d3c88b94f5962bdb
Zaher ABBAS <z.abbas83@yahoo.com>
2026-03-04 10:02:56 +0100

    update dashboard: add conditional styling for favorite recipes with highlighted borders and placeholder spacing

--- src/View/dashboard.php
+++ src/View/dashboard.php
@@ -31,9 +31,11 @@ $user = isset($_SESSION['userName']) ? $_SESSION['userName'] : "";
         <div class="row row-cols-lg-3 row-cols-md-2 row-cols-sm-1 g-4 p-4 justify-content-center">
             <?php foreach ($recipes as $recipe): ?>
                 <div class="col">
-                    <div class="card h-100 recipe-card bg-sage-light text-forest border border-secondary-subtle border-start-0 rounded-end border-4 mb-3 p-4">
+                    <div class="<?= (isset($recipe['isFavorite']) && $recipe['isFavorite']) ? 'border border-3 border-warning' : 'border border-secondary-subtle border-start-0 rounded-end border-4' ?> card h-100 recipe-card bg-sage-light text-forest mb-3 p-4">
                       <?php if (isset($recipe['isFavorite']) && ($recipe['isFavorite'] === true)): ?>
                           <h3>&#11088;</h3>
+                      <?php else: ?>
+                      <h3 class="p-3"></h3>
                           <?php endif; ?>
                         <?php if ($recipe['image'] === ''): ?>
                             <img src="./../View/img/recipe_placeholder.png"
