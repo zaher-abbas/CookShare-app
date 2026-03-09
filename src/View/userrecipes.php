29aaab7b2c0b95619d125dd990e1211363ed4016
Zaher ABBAS <z.abbas83@yahoo.com>
2026-03-09 15:39:06 +0100

    update userrecipes view: adjust main heading emoji for improved clarity and consistency

--- src/View/userrecipes.php
+++ src/View/userrecipes.php
@@ -3,7 +3,7 @@
 
 ?>
 <main>
-    <h1 class="text-center my-5 fw-bold">&#128100; My Recipes &#129379;</h1>
+    <h1 class="text-center my-5 fw-bold">&#129379; My Recipes</h1>
     <?php if ($userRecipes): ?>
     <div class="container">
         <section class="list-group">
