6d857907b417a924a9b709b2680e7309de332e5b
Zaher ABBAS <z.abbas83@yahoo.com>
2026-03-11 15:31:52 +0100

    fix Recipe model: correct table alias usage in SQL queries to ensure consistency and proper functionality

--- src/Model/Recipe.php
+++ src/Model/Recipe.php
@@ -37,7 +37,7 @@ class Recipe
 
     public function getRecipeById(int $id): array|null
     {
-        $query = "SELECT recipes.*, firstname, lastname, photo FROM recipes JOIN users u ON u.id = recipe.user_id WHERE recipes.id = :id";
+        $query = "SELECT recipes.*, firstname, lastname, photo FROM recipes JOIN users u ON u.id = recipes.user_id WHERE recipes.id = :id";
         $statement = $this->db->prepare($query);
         $statement->bindValue(':id', $id);
         $statement->execute();
@@ -46,7 +46,7 @@ class Recipe
 
     public function searchRecipeByName(string $query): array|null
     {
-        $sql = "SELECT recipes.*, firstname, lastname FROM recipe JOIN users u ON u.id = recipe.user_id WHERE recipes.name LIKE :query";
+        $sql = "SELECT recipes.*, firstname, lastname FROM recipe JOIN users u ON u.id = recipes.user_id WHERE recipes.name LIKE :query";
         $statement = $this->db->prepare($sql);
         $statement->bindValue(':query', '%' . $query . '%');
         $statement->execute();
