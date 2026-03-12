b1149844ffcb6b5db514fb61ae2b55ab95ae77a8
Zaher ABBAS <z.abbas83@yahoo.com>
2026-03-12 14:44:14 +0100

    update index view: change language from French to English in HTML tag

--- src/View/index.php
+++ src/View/index.php
@@ -12,7 +12,7 @@ $action = $_GET['action'] ?? 'home';
 ?>
 
 <!doctype html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
