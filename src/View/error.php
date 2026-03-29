c57323c92e3e5b334b0fe60be16372395b72317d
Zaher ABBAS <z.abbas83@yahoo.com>
2026-03-30 01:21:54 +0200

    update error styling: adjust font size and add icon for improved user feedback

--- src/View/error.php
+++ src/View/error.php
@@ -1,6 +1,6 @@
 <main class="flex-grow-1 d-flex align-items-center justify-content-center">
     <section class="container my-4">
-        <div class="alert alert-danger text-center fs-3" role="alert">
-            Something went wrong while loading this page. Please try again later.
+        <div class="alert alert-danger text-center fs-4" role="alert">
+            &#10060; Something went wrong while loading this page. Please try again later.
         </div></section>
 </main>
