3c42a7a494fbbafd2a5943a6004d1a408287a4e8
Zaher ABBAS <z.abbas83@yahoo.com>
2026-04-01 10:21:51 +0200

    enhance welcome page text with more engaging and visually descriptive content

--- src/View/welcome.php
+++ src/View/welcome.php
@@ -6,13 +6,15 @@
             <div class="row justify-content-center text-center">
                 <div class="col-lg-8">
                     <h1 class="display-4 fw-bold mb-4">Welcome to CookShare &#127869;</h1>
-                    <p class="lead my-5">View recipes from all over the world, shared by everyone, you can
-                        add yours too!</p>
+                    <p class="lead my-5">Discover tasty recipes 🥗 from kitchens around the world 🌍, share your signature
+                        dishes 👨‍🍳, and turn every meal into something unforgettable ✨
+                    </p>
                     <p class="lead mb-5">Try it...it is Fun!</p>
                     <div class="d-flex gap-3 justify-content-center flex-wrap">
                         <a href="index.php?action=register"
                            class="btn btn-light btn-lg px-4 py-2 me-4 btnHero">Register</a>
-                        <a href="index.php?action=login" class="btn btn-outline-light btn-lg px-4 py-2 btnHero">Login</a>
+                        <a href="index.php?action=login"
+                           class="btn btn-outline-light btn-lg px-4 py-2 btnHero">Login</a>
                     </div>
                 </div>
             </div>
