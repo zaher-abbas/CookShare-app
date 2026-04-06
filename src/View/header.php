f7a120d4d8138c6149583d4888980091c4b89002
Zaher ABBAS <z.abbas83@yahoo.com>
2026-04-06 18:32:12 +0200

    refine navbar item styles for improved responsiveness and alignment

--- src/View/header.php
+++ src/View/header.php
@@ -30,7 +30,7 @@ $isConnected = isset($_SESSION['isConnected']) && $_SESSION['isConnected'] === t
                 if ($isConnected && $_SESSION['userRole'] === 'user') {
                     echo "<ul class='navbar-nav me-auto mb-2 mb-lg-0 mb-md-0'>
                         <li class='nav-item me-lg-3 mb-2 mb-md-2 mb-lg-0'>
-                            <a class='btn btn-outline-success border-bottom border-3 text-light btn-md rounded-bottom-5 p-2 w-100 fw-bold' aria-current='page' href='index.php?action=home'>&#127968; Home</a>
+                            <a class='btn btn-outline-success border-bottom border-3 text-light btn-md rounded-bottom-5 p-2 w-100 fw-bold' href='index.php?action=home'>&#127968; Home</a>
                         </li>
                         <li class='nav-item mb-2 mb-lg-0 mb-md-2'>
                             <a class='btn btn-outline-primary border-bottom border-3 text-light btn-md rounded-bottom-5 p-2 w-100 fw-bold' href='index.php?action=addrecipe'>&#10133; Add Recipe</a>
@@ -71,7 +71,7 @@ $isConnected = isset($_SESSION['isConnected']) && $_SESSION['isConnected'] === t
                         </ul>
                     <?php else: ?>
                         <ul class='navbar-nav mt-2 mt-lg-0 mt-md-0 d-flex justify-content-center align-items-center'>
-                            <li class='nav-item mt-sm-2 mt-lg-0 mt-md-0 mb-2 mb-lg-0 mb-md-0'>
+                            <li class='nav-item mt-sm-2 mt-lg-0 mt-md-0 mb-2 mb-lg-0 mb-md-0 profileBtn'>
                       <a href='index.php?action=profile'
                          class='btn btn-outline-primary text-light btn-md border-0 p-2 text-decoration-none mt-0 w-100 fw-bold'>
                             <img src="<?= BASE_URL . '/img/' . htmlspecialchars($_SESSION['userPhoto']) ?>"
@@ -79,7 +79,7 @@ $isConnected = isset($_SESSION['isConnected']) && $_SESSION['isConnected'] === t
                                  alt="User Profile Picture">
                           <?= $user ?></a>
                             </li>
-                         <li class='nav-item ms-xxl-4 ms-xl-3 ms-lg-3 mt-3 mt-md-3 mt-lg-0'>
+                         <li class='nav-item ms-xxl-4 ms-xl-3 ms-lg-3 mt-3 mt-md-3 mt-lg-0 logoutBtn'>
                                 <a class='btn btn-outline-danger btn-md p-2 rounded-2 mt-0 w-100 fw-bold'
                                    href='index.php?action=logout'>Logout &#8618;</a>
                         </li>
