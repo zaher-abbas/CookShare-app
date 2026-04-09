8e14578c1083183a7575445ae2a3d8ea6986dd00
Mouhammad ABBAS <z.abbas83@yahoo.com>
2026-04-09 15:15:00 +0200

    Refactored the welcome page layout for improved structure and readability.

--- src/View/welcome.php
+++ src/View/welcome.php
@@ -1,25 +1,24 @@
 <?php
 ?>
 <main class="flex-grow-1 d-flex justify-content-center align-items-center">
-    <section class="hero-section p-3 mx-2 my-2 p-lg-5 p-md-3 p-sm-4 rounded-5">
-        <div class="container">
-            <div class="row justify-content-center text-center">
-                <div class="col-lg-8 col-md-10 col-sm-10">
-                    <h1 class="display-2 fw-bold mb-4">Welcome to CookShare &#127869;</h1>
-                    <p class="lead my-5">Discover tasty recipes 🥗 from kitchens around the world 🌍, share your signature
-                        dishes 👨‍🍳, and turn every meal into something unforgettable ✨
-                    </p>
-                    <p class="lead mb-5">Try it...it is Fun!</p>
-                    <div class="d-flex gap-3 justify-content-center align-items-center flex-wrap">
-                        <a href="index.php?action=register"
-                           class="btn btn-light btn-lg px-4 py-2 btnHero">Register</a>
-                        <a href="index.php?action=login"
-                           class="btn btn-outline-light btn-lg px-4 py-2 btnHero">Login</a>
-                    </div>
+    <div class="row justify-content-center text-center my-3">
+        <div class="col-10 col-sm-10 col-md-8 col-lg-8">
+            <section class="hero-section p-3 mx-2 my-2 p-lg-5 p-md-3 p-sm-4 rounded-5">
+                <h1 class="display-2 fw-bold mb-4">Welcome to CookShare &#127869;</h1>
+                <p class="lead my-5">Discover tasty recipes 🥗 from kitchens around the world 🌍, share your signature
+                    dishes 👨‍🍳, and turn every meal into something unforgettable ✨
+                </p>
+                <p class="lead mb-5">Try it...it is Fun!</p>
+                <div class="d-flex gap-3 justify-content-center align-items-center flex-wrap">
+                    <a href="index.php?action=register"
+                       class="btn btn-light btn-lg px-4 py-2 btnHero">Register</a>
+                    <a href="index.php?action=login"
+                       class="btn btn-outline-light btn-lg px-4 py-2 btnHero">Login</a>
                 </div>
-            </div>
+
+            </section>
         </div>
-    </section>
+    </div>
 </main>
 <?php if (isset($_COOKIE['loggedOut']) && $_COOKIE['loggedOut'] === 'true'): ?>
     <script>
