b93cde017683e7fa4cac03c4c9c7c4fd7eaafb7d
Mouhammad ABBAS <z.abbas83@yahoo.com>
2026-04-10 13:06:06 +0200

    Update edit recipe view and styles: Adjusted form and title classes for better layout consistency and responsiveness.

--- src/View/editrecipe.php
+++ src/View/editrecipe.php
@@ -7,11 +7,11 @@ $action = isset($_GET['action']) ? $_GET['action'] : '';
 <main class="flex-grow-1 d-flex align-items-center justify-content-center">
     <section class="container my-4">
         <?php if ($action == 'addrecipe'): ?>
-            <h3 class="text-center alert alert-light w-75 mx-auto">&#10133; Add New Recipe</h3>
+            <h3 class="edit-title text-center alert alert-light w-75 mx-auto">&#10133; Add New Recipe</h3>
         <?php elseif ($action == 'updaterecipe'): ?>
-            <h3 class="text-center alert alert-light w-75 mx-auto">&#9998; Edit Recipe</h3>
+            <h3 class="edit-title text-center alert alert-light w-75 mx-auto">&#9998; Edit Recipe</h3>
         <?php endif; ?>
-        <form class="edit-form p-3 pb-1 p-md-4 pb-md-1 p-lg-5 pb-lg-1 rounded-3 w-100 mx-auto" method="post" action="" enctype="multipart/form-data">
+        <form class="edit-form p-3 pb-1 p-md-4 pb-md-1 p-lg-5 pb-lg-1 rounded-3 w-75 mx-auto" method="post" action="" enctype="multipart/form-data">
             <?php
             if (!empty($errors))
             foreach ($errors as $error)
