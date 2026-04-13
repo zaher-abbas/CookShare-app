3e41e98da4093ab405a0a2be2c7e1c03e1cc9fec
Mouhammad ABBAS <z.abbas83@yahoo.com>
2026-04-13 17:02:55 +0200

    Refine navbar padding and update button hover/focus styles for consistent UI appearance

--- src/View/header.php
+++ src/View/header.php
@@ -17,8 +17,8 @@ $isConnected = isset($_SESSION['isConnected']) && $_SESSION['isConnected'] === t
     </script>
     <?php unset($_SESSION['toast']); endif; ?>
 <header>
-    <nav class="navbar navbar-expand-lg p-2 p-md-3 p-lg-4 text-light">
-        <div class="container-fluid  d-flex flex-column flex-md-row align-items-center justify-content-md-between">
+    <nav class="navbar navbar-expand-lg p-2 p-md-3 p-lg-4">
+        <div class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-md-between">
             <a class="navbar-brand text-light fs-1 fw-bold logo" href="index.php"><span class="logo-icon">🍽️</span> CookShare</a>
             <button class="navbar-toggler fw-bold mt-2 mb-3 mt-md-0" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
