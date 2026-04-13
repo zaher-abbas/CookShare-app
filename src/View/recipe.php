006aa079fb7da6f1c481fe5b455c294d80fb38dc
Mouhammad ABBAS <z.abbas83@yahoo.com>
2026-04-13 13:56:38 +0200

    Update recipe view: replace hardcoded calendar emoji with HTML entity for consistency

--- src/View/recipe.php
+++ src/View/recipe.php
@@ -65,8 +65,8 @@
                                             <?= htmlspecialchars($recipe['firstname']) . ' ' . htmlspecialchars($recipe['lastname']) ?>
                                         </span>
                                     </span>
-                                    <span>on 📅<time class="badge bg-light text-secondary ms-1">
-                                            <?= htmlspecialchars(date('d/m/Y', strtotime($recipe['created_at']))) ?>
+                                    <span>on <time class="badge bg-light text-secondary">
+                                            &#128198; <?= htmlspecialchars(date('d/m/Y', strtotime($recipe['created_at']))) ?>
                                         </time>
                                     </span>
                                 </span>
