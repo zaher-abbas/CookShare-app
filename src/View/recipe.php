f03cf0ff5271407fc23cd37c11e1731494ec3499
Zaher ABBAS <z.abbas83@yahoo.com>
2026-04-14 18:56:19 +0200

    refine card-body styles in recipe view for improved spacing and visual hierarchy

--- src/View/recipe.php
+++ src/View/recipe.php
@@ -83,7 +83,7 @@
                             <?php endif; ?>
                         </div>
                     </div>
-                    <div class="card-body px-0 px-md-2 px-lg-4">
+                    <div class="card-body px-0 mt-4">
                         <h2 class="mb-3 ">&#128195; Instructions</h2>
                         <div class="border-start border-4 border-success ps-3 py-3 bg-light rounded-3 shadow-sm">
                         <p class="card-text"><?= htmlspecialchars($recipe['description']) ?></p>
