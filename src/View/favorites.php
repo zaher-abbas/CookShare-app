268ddfbdade517fb6c4231619dcb2254cc40878b
Zaher ABBAS <z.abbas83@yahoo.com>
2026-04-15 17:14:14 +0200

    refine styles and layout in dashboard and favorites views for improved consistency, spacing, and visual hierarchy

--- src/View/favorites.php
+++ src/View/favorites.php
@@ -8,22 +8,36 @@
         <h1 class="text-center my-5 fw-bold">&#11088; My Favorite Recipes &#11088;</h1>
         <?php if ($favoriteRecipes): ?>
         <div class="container page-favorites">
-            <div class="row row-cols-lg-3 row-cols-md-2 row-cols-sm-1 g-4 p-2 p-lg-4 p-md-3 justify-content-center">
+            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 p-2 p-md-3 p-lg-4 justify-content-center">
                 <?php foreach ($favoriteRecipes as $recipe): ?>
-                    <article class="col p-0 ">
+                    <article class="col">
                         <div class="card h-100 recipe-card bg-sage-light text-forest border border-secondary-subtle border-start-0 rounded-end border-4 mb-3 p-4">
                             <img src="<?= BASE_URL . '/img/' . $recipe['image'] ?>"
                                  class="card-img-top rounded-start w-100 fixed-img"
                                  alt="">
-                            <div class="card-body">
+                            <div class="card-body d-flex flex-column justify-content-between pb-0">
                                 <h5 class="card-title fw-bold"><?= htmlspecialchars($recipe['name']) ?></h5>
-                                <p class="card-text">Contributed by<span
+                                <div>
+                                    <span class="badge rounded-pill text-bg-primary fs-6 p-2 mt-1 me-2">&#8987; <?= htmlspecialchars($recipe['duration']) ?> min</span>
+                                    <?php switch ($recipe['difficulty']) {
+                                        case 'Easy':
+                                            echo '<span class="badge rounded-pill text-bg-success fs-6 p-2 mb-3">&#127919; Easy</span>';
+                                            break;
+                                        case 'Medium':
+                                            echo '<span class="badge rounded-pill text-bg-warning fs-6 p-2 mb-3">&#127919; Medium</span>';
+                                            break;
+                                        case 'Hard':
+                                            echo '<span class="badge rounded-pill text-bg-danger fs-6 p-2 mb-3">&#127919; Hard</span>';
+                                            break;
+                                    } ?>
+                                </div>
+                                <p class="card-text">Contributed by <span
                                             class="badge bg-light text-secondary border fs-6">
                                             &#128105;&#8205;&#127859; <?= htmlspecialchars($recipe['firstname']) . ' ' . htmlspecialchars($recipe['lastname']) ?>
                                         </span></p>
                                 <div class="text-center my-4">
                                     <a href="index.php?action=recipe&id=<?= $recipe['id'] ?>"
-                                       class="btn btn-success w-60">Check
+                                       class="btn btn-success w-100">Check
                                         this Recipe!</a>
                                 </div>
                             </div>
