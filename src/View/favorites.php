ca2f27ef26f73076a4aafc4c0e5d74aa64f5d2b6
Mouhammad ABBAS <z.abbas83@yahoo.com>
2026-04-17 07:05:16 +0200

    Update date display in `favorites.php` footer to include calendar icon for improved clarity

--- src/View/favorites.php
+++ src/View/favorites.php
@@ -41,9 +41,9 @@
                                         this Recipe!</a>
                                 </div>
                             </div>
-                            <div class="card-footer">                                    <time class="fs-6"><?= date('d/m/Y H:i', strtotime($recipe['created_at'])) ?></time>
-
-                                <small class="text-body-secondary">
+                            <div class="card-footer">
+                                <small class="text-body-secondary">&#128197; Date added
+                                    <time class="fs-6"><?= date('d/m/Y H:i', strtotime($recipe['created_at'])) ?></time>
                                 </small>
                             </div>
                         </div>
